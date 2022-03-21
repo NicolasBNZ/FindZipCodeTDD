d2ff6b76bd363ab6312439d74274f641108366a4
NicolasBNZ <nicolas.benzadon@gmail.com>
2022-03-21 20:55:59 +0100

    :art: the main branch is clean, we can create others branches

--- findzipcodetdd/src/components/App/index.tsx
+++ findzipcodetdd/src/components/App/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Test from "../Testpourvoir/index"
+
 
 
 function App() {
   return (
     <div className="App">
       FIND A ZIP CODE IN TDD
-      <Test/>
     </div>
   );
 }
